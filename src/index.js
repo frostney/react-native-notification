66f0fc57936a2a54b8eac84eca908e4347eb38c0
sarovin <sarovin86@gmail.com>
2016-05-03 22:24:30 +0200

    Notification without visible state

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, {
   Component,
+  StyleSheet,
   PropTypes,
   Text,
   Animated,
@@ -8,116 +9,89 @@ import React, {
 
 const Screen = Dimensions.get('window');
 
-class Notification extends Component {
-  static propTypes = {
-    children: PropTypes.node,
-    visible: PropTypes.bool,
-  };
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    bottom: 35,
+    width: Screen.width - 80,
+    left: 40,
+    right: 40,
+    backgroundColor: '#444',
+    alignItems: 'center',
+    padding: 6,
+    borderRadius: 12,
+    shadowColor: '#000',
+    shadowOpacity: 0.5,
+    shadowRadius: 1,
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+  },
+  message: {
+    color: '#fff',
+    fontSize: 12,
+    textAlign: 'center',
+  },
+});
 
-  static defaultProps = {
-    visible: true,
-  };
+const propTypes = {
+  fadeTime: PropTypes.number,
+  minOpacity: PropTypes.number,
+  maxOpacity: PropTypes.number,
+  message: PropTypes.string,
+};
+
+const defaultProps = {
+  fadeTime: 500,
+  minOpacity: 0.0,
+  maxOpacity: 0.9,
+  message: '',
+};
+
+class Notification extends Component {
 
   constructor(props) {
     super(props);
 
     this.state = {
-      opacityValue: new Animated.Value(Notification.minOpacity),
+      opacityValue: new Animated.Value(this.props.minOpacity),
     };
   }
 
-  componentDidMount() {
-    if (this.props.visible) {
-      this.fadeIn();
-    }
+  componentWillReceiveProps() {
+    this.fadeIn();
+
+    setTimeout(() => {
+      this.fadeOut();
+    }, 3000);
   }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.visible && !this.props.visible) {
-      this.fadeIn();
-    } else {
-      if (!nextProps.visible && this.props.visible) {
-        this.fadeOut();
-      }
-    }
-  }
-
-  shouldComponentUpdate(nextProps) {
-    // TODO: Compare the messages with each other
-    /* if (this.props.message !== nextProps.message) {
-      return true;
-    }*/
-
-    if (this.props.visible !== nextProps.visible) {
-      return true;
-    }
-
-    // TODO: Is there a reliable way not use `__getValue` and something else that may not be as unstable
-    if (this.state.opacityValue.__getValue() !== this.state.opacityValue.__getValue()) {
-      return true;
-    }
-
-    return false;
-  }
-
-  static fadeTime = 500;
-  static minOpacity = 0.0;
-  static maxOpacity = 0.9;
-
-  static styles = {
-    container: {
-      position: 'absolute',
-      bottom: 35,
-      width: Screen.width - 80,
-      left: 40,
-      right: 40,
-      backgroundColor: '#444',
-      alignItems: 'center',
-      padding: 6,
-      opacity: Notification.minOpacity,
-      borderRadius: 12,
-      shadowColor: '#000',
-      shadowOpacity: 0.5,
-      shadowRadius: 1,
-      shadowOffset: {
-        width: 0,
-        height: 1,
-      },
-    },
-    message: {
-      color: '#fff',
-      fontSize: 12,
-      textAlign: 'center',
-    },
-  };
 
   fadeIn = () => {
     Animated.timing(this.state.opacityValue, {
-      duration: Notification.fadeTime,
-      toValue: Notification.maxOpacity,
+      duration: this.props.fadeTime,
+      toValue: this.props.maxOpacity,
     }).start();
   }
 
   fadeOut = () => {
     Animated.timing(this.state.opacityValue, {
-      duration: Notification.fadeTime,
-      toValue: Notification.minOpacity,
+      duration: this.props.fadeTime,
+      toValue: this.props.minOpacity,
     }).start();
   }
 
   render() {
-    const message = this.props.children;
-
-    if (!message) {
-      return null;
-    }
 
     return (
-      <Animated.View style={[Notification.styles.container, {opacity: this.state.opacityValue}]}>
-        <Text style={Notification.styles.message}>{message}</Text>
+      <Animated.View style={[styles.container, { opacity: this.state.opacityValue }]}>
+        <Text style={styles.message}>{this.props.message}</Text>
       </Animated.View>
     );
   }
 }
+
+Notification.propTypes = propTypes;
+Notification.defaultProps = defaultProps;
 
 export default Notification;
